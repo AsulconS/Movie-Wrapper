68e8ce5cdb802d90d223d647173db420b748ef61
AsulconS <rolandobedregal321@gmail.com>
2019-09-09 02:53:34 -0500

    little fixes

--- include/movieWrapper.hpp
+++ include/movieWrapper.hpp
@@ -4,10 +4,14 @@
 
 #include "downloader/downloader.hpp"
 
+//#include "movie/movie.hpp"
+
 #ifdef MW_IMPLEMENTATION
     #include "list/linkedList.inl"
     #include "list/unorderedList.inl"
     #include "list/orderedList.inl"
 
     #include "downloader/downloader.inl"
+
+    //#include "movie/movie.inl"
 #endif
